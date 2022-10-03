707bb2ae4154985cf08ab11061d14e31f4d2133d
MacenaAndre <andremacenacosta@gmail.com>
2022-10-03 16:48:09 -0300

    Post categories, INSERT database & validations

--- src/controllers/categories.controllers.js
+++ src/controllers/categories.controllers.js
@@ -1,13 +1,51 @@
 import connection from "../database.connection/pg.js"
 import joi from "joi";
+import { stripHtml } from "string-strip-html";
+
+const categorieSchema = joi.object({
+	name: joi.string().min(1).required()
+});
 
 const getCategories = async (req, res) => {
     try {
         const categories = await connection.query(`SELECT * FROM categories`);
-        res.status(200).send(categories.rows);
+        return res.status(200).send(categories.rows);
     } catch (error) {
-        res.send(error.message);
+        return res.status(500).send(error.message);
     }
 };
 
-export { getCategories }
+const postCategories = async (req, res) => {
+    let name = req.body.name
+
+    const validation = categorieSchema.validate({name});
+
+    if (validation.error) {
+		const errors = validation.error.details.map((error) => error.message);
+		return res.status(400).send(errors);
+	} else {
+        name = stripHtml(name).result.trim();
+    }
+
+    if(name.length === 0 ) {
+        return res.sendStatus(400);
+    }
+
+    try {
+        const categories = await connection.query(`SELECT name FROM categories WHERE name='${name}'`)
+        console.log(categories.rows);
+
+        if(categories.rows.length > 0) {
+            return res.sendStatus(409);
+        }
+
+        await connection.query(`INSERT INTO categories (name) VALUES ($1)`, [name]);
+        return res.sendStatus(201);
+
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+
+};
+
+export { getCategories, postCategories }
--- src/routers/categories.routes.js
+++ src/routers/categories.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getCategories } from "../controllers/categories.controllers.js";
+import { getCategories, postCategories } from "../controllers/categories.controllers.js";
 
 const categoriesRouter = express.Router();
 
 categoriesRouter.get("/categories", getCategories);
+
+categoriesRouter.post("/categories", postCategories);
 
 export { categoriesRouter };
